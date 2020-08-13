deb1f662205e54c05f69be1e1ffa78c9dac31a29
Viktor Mikhail Bitencourt <vbprogramacoes@gmail.com>
2020-08-13 20:46:35 -0300

    Projeto base completo

--- index.php
+++ index.php
@@ -1,1 +1,19 @@
-index.php
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Teste Celero</title>
+	<link rel="stylesheet" type="text/css" href="./assets/css/style.css">
+	<script type="text/javascript" src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+</head>
+<body>
+	<script type="text/javascript">
+		$.ajax({
+		  url: 'https://randomuser.me/api/',
+		  dataType: 'json',
+		}).done(function(data){
+			console.log(data);
+			console.log(data.results[0].name.first);
+		});
+	</script>
+</body>
+</html>
