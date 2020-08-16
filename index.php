96771d5e00d516c020905745f7d67ee8dd966cc9
Viktor Mikhail Bitencourt <vbprogramacoes@gmail.com>
2020-08-16 17:49:14 -0300

    Arrumando detalhes das tags

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<title>Teste Celero - Web Develope</title>
-	<link rel="stylesheet" type="text/css" href="./assets/css/style.css">
+	<link rel="stylesheet" type="text/css" href="./assets/css/style.css" />
 </head>
 <body>
 	<div class="body">
